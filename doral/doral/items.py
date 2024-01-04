47e257ba8890f1a9d37077a919493e9da04da3ae
Jose M. Gomez <gomezjosem@live.com>
2024-01-03 22:19:23 -0500

    added business info fields

--- doral/doral/items.py
+++ doral/doral/items.py
@@ -5,8 +5,14 @@
 
 import scrapy
 
-
-class DoralItem(scrapy.Item):
+class BusinessItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    
+    # business info...        
+    business_name: str = scrapy.Field()
+    industry: str = scrapy.Field()
+    offer: str = scrapy.Field()
+    website: str = scrapy.Field()
+    phone: set[str] = scrapy.Field()
+    contact_name: str = scrapy.Field()
