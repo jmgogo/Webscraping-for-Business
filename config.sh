1e8098f011621def5a34ee083f2cbebbbf745a41
Jose M. Gomez <gomezjosem@live.com>
2024-01-16 22:10:49 +0000

    removed unecessary use of cat command

--- config.sh
+++ config.sh
@@ -4,11 +4,11 @@ echo # add white space for formatting
 echo "Configuring Git..."
 echo "-----------------------------------------"
 # config username and print for confirmation
-echo "User: $(cat credentials.txt | grep User | cut -d : -f 2)"
-cat credentials.txt | grep User | cut -d : -f 2 | git config --global user.name
+echo "User: $(grep User credentials.txt | cut -d : -f 2)"
+grep User credentials.txt | cut -d : -f 2 | git config --global user.name
 # config email and print for confirmation
-echo "Email: $(cat credentials.txt | grep Email | cut -d : -f 2)" 
-cat credentials.txt | grep Email | cut -d : -f 2 | git config --global user.email
+echo "Email: $(grep Email credentials.txt | cut -d : -f 2)" 
+grep Email credentials.txt | cut -d : -f 2 | git config --global user.email
 echo # add white space for formatting
 
 echo "Displaying OS Info..."
